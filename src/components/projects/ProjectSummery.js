ea250c1e2185d41762ee379f0751a99615ca7a33
Ali <alimansour97@gmail.com>
2018-09-20 01:51:48 -0600

    add projects in summery and export functions

---
+++ src/components/projects/ProjectSummery.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const ProjectSummery = ()=>{
+    return(
+    <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project title</span>
+          <p>posted by me</p>
+          <p className="grey-text">today</p>
+        </div>
+        </div>
+    )
+}
+
+export default ProjectSummery
